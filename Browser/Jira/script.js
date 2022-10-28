65efadadef90ebbfa124b3eec80a42f193d953c3
seehimanshu <himanshu.thakur2812@gmail.com>
2022-10-29 04:15:30 +0530

    JIRA started

---
+++ Browser/Jira/script.js
@@ -0,0 +1,26 @@
+const addBtn=document.querySelector(".add-btn");
+const modalCont=document.querySelector(".modal-cont");
+
+let isModalPresent=false;
+addBtn.addEventListener('click',function(){
+    if(!isModalPresent){
+        modalCont.style.display="flex";//modal add hogya screen pr
+
+    }
+    else{
+        modalCont.style.display="none";
+
+    }
+    isModalPresent=!isModalPresent;
+})
+
+const allPriorityColor=document.querySelectorAll(".priority-color");
+
+allPriorityColor.forEach(function(colorElement){
+    colorElement.addEventListener("click",function(){
+        allPriorityColor.forEach(function(priorityColorElem){
+            priorityColorElem.classList.remove("active");
+        });
+        colorElement.classList.add("active");
+    });
+});
